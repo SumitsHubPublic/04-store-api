9f16e09e9be21a4d0301696d0850d33b09f96183
Sumit Paul <pauls1317@gmail.com>
2024-08-30 10:17:06 +0530

    added cors support

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require('express-async-errors');
 
+const cors = require("console");
+
 const express = require('express');
 const connectDB = require('./db/connect');
 
@@ -11,6 +13,8 @@ const errorHandler = require('./middleware/error-handler');
 const productsRouter = require('./routes/products');
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('<h1>Store Api</h1><a href="/api/v1/products">products route</a>');
